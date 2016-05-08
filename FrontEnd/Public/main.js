55b2a5e78758c80cb14bba5589d7a61d6cddbff0
Chris Sullivan <codemastermd@gmail.com>
2016-05-07 23:50:06 -0400

    Made the different location types work

--- FrontEnd/Public/main.js
+++ FrontEnd/Public/main.js
@@ -14,7 +14,7 @@ var map;
       var mapOptions = {
         center: new google.maps.LatLng(36.1699,115.1398),
         //center: new google.maps.LatLng(51.503454,-0.119562),
-        zoom: 8,
+        zoom: 14,
         mapTypeId: google.maps.MapTypeId.ROADMAP
       };
 
@@ -134,14 +134,15 @@ var map;
     // CROSS SITE XHR ends here
 
 
-    var beaches = [
-      ['Bondi Beach', -33.890542, 151.274856, 4],
-      ['Coogee Beach', -33.923036, 151.259052, 5],
-      ['Cronulla Beach', -34.028249, 151.157507, 3],
-      ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
-      ['Maroubra Beach', -33.950198, 151.259302, 1]
-    ];
+    // var beaches = [
+    //   ['Bondi Beach', -33.890542, 151.274856, 4],
+    //   ['Coogee Beach', -33.923036, 151.259052, 5],
+    //   ['Cronulla Beach', -34.028249, 151.157507, 3],
+    //   ['Manly Beach', -33.80010128657071, 151.28747820854187, 2],
+    //   ['Maroubra Beach', -33.950198, 151.259302, 1]
+    // ];
 
+    var locations = [];
      // MARKER IMAGES
      // http://lalude.com/adesina/HelpingHandSupport/iconBurger1.png
 
@@ -151,9 +152,15 @@ var map;
       jsonData = JSON.parse(jsonData);
 
       //console.log(obj);
-      for(i in jsonData){
-        console.log(i);
+      for(i in jsonData.data){
+        console.log(jsonData.data[i]);
+        locations.push([jsonData.data[i].name,
+                        jsonData.data[i].lat,
+                        jsonData.data[i].long,
+                        jsonData.data[i].service_type,
+                        parseInt(i)+1])
       }
+      console.log(locations)
 
       // Adds markers to the map.
 
@@ -162,35 +169,47 @@ var map;
 
       // Origins, anchor positions and coordinates of the marker increase in the X
       // direction to the right and in the Y direction down.
-      var image = {
-        // url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+      var bed = {
         url: 'http://lalude.com/adesina/HelpingHandSupport/iconBed1.png',
-        // url: 'http://lalude.com/adesina/HelpingHandSupport/iconBurger2.png',
-        // url: 'http://lalude.com/adesina/HelpingHandSupport/beachflag.png',
-        // This marker is 20 pixels wide by 32 pixels high.
-        //size: new google.maps.Size(20, 32),
         size: new google.maps.Size(32, 32),
-        // The origin for this image is (0, 0).
         origin: new google.maps.Point(0, 0),
-        // The anchor for this image is the base of the flagpole at (0, 32).
         anchor: new google.maps.Point(0, 32)
       };
-      // Shapes define the clickable region of the icon. The type defines an HTML
-      // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-      // The final coordinate closes the poly by connecting to the first coordinate.
+      var food = {
+        url: 'http://lalude.com/adesina/HelpingHandSupport/iconFood1.png',
+        size: new google.maps.Size(32, 32),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(0, 32)
+      }
+      var cross = {
+        // url: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png',
+        url: 'http://lalude.com/adesina/HelpingHandSupport/iconDoctor1.png',
+        size: new google.maps.Size(32, 32),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(0, 32)
+      }
       var shape = {
         coords: [1, 1, 1, 20, 18, 20, 18, 1],
         type: 'poly'
       };
-      for (var i = 0; i < beaches.length; i++) {
-        var beach = beaches[i];
+      for (var i = 0; i < locations.length; i++) {
+        var imageType;
+        var location = locations[i];
+        if(location[3] == 1){
+          imageType = bed;
+        } else if(location[3] == 2){
+          imageType = food;
+        } else if(location[3] == 3){
+          imageType = cross;
+        }
+
         var marker = new google.maps.Marker({
-          position: {lat: beach[1], lng: beach[2]},
+          position: {lat: location[1], lng: location[2]},
           map: map,
-          icon: image,
+          icon: imageType,
           shape: shape,
-          title: beach[0],
-          zIndex: beach[3]
+          title: location[0],
+          zIndex: location[4]
         });
       }
     }
