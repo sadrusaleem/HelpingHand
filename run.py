e0c857b09011a6fcdf8d2ff42a12f7208b8c352f
sadrusaleem <sadruddinsaleem17@gmail.com>
2016-05-08 00:35:20 +0000

    cloud9 hosting

--- run.py
+++ run.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Nothing works!'
+    return 'AHHH'
 
-#if __name__ == '__main__':
-    #app.run()
+if __name__ == "__main__":
+    app.run(debug=True, 
+            host=os.getenv('IP', '0.0.0.0'),
+            port = int(os.getenv('PORT', '8080')))
