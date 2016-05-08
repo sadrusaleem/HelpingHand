a99cdfccfad34d64bd7e58c8b12d7e6dccbedadf
Sadruddin Saleem <ssaleem@ssaleem.local>
2016-05-07 20:43:28 -0400

    dummy api layer

--- run.py
+++ run.py
@@ -5,7 +5,18 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'AHHH'
+    return {'name': 'Test Shelter',
+            'address': '120 East 32nd St, New York',
+            'lat': 40.745374368133696,
+            'long': -73.98135204156267
+            'service_type': 1,
+            'phone': '9545361686'
+    }
+
+@app.route('/locations/input')
+def hello_world(input):
+    print input
+    return input
 
 if __name__ == "__main__":
     app.run(debug=True, 
