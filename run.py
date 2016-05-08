5eaf6e95563d3ce1778586b60d5704fd986ea5c9
sadrusaleem <sadruddinsaleem17@gmail.com>
2016-05-08 00:53:11 +0000

    dummy data

--- run.py
+++ run.py
@@ -1,20 +1,23 @@
-from flask import Flask
+from flask import Flask, jsonify
 import os
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return {'name': 'Test Shelter',
+    data = [{'name': 'Test Shelter',
             'address': '120 East 32nd St, New York',
             'lat': 40.745374368133696,
-            'long': -73.98135204156267
+            'long': -73.98135204156267,
             'service_type': 1,
             'phone': '9545361686'
-    }
+    }]
+    
+    return json.dumps(data)
 
 @app.route('/locations/input')
-def hello_world(input):
+def hello_world2(input):
     print input
     return input
 
