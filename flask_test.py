7e54e12938a339571e15c0428d1afeb01166dfe4
trequartista_10 <mailkuldeepm@gmail.com>
2016-05-07 21:04:24 -0400

    sd

--- flask_test.py
+++ flask_test.py
@@ -17,9 +17,69 @@ cache.set('counter',100)
 
 app = Flask(__name__)
 
+import requests # pip install requests
+import json
+
+class Shelter(object):
+    name=""
+    address=""
+    lat=""
+    long=""
+    service_type=1
+    daysOfWeek=""
+    startTime=""
+    endTime=""
+    phone=""
+        
+    # The class "constructor" - It's actually an initializer 
+    def __init__(self, name, address, lat, long, service_type,daysofweek,starttime,endtime,phone):
+        self.name = name
+        self.address = address
+        self.lat = lat
+        self.long = long
+        self.service_type = service_type
+        self.daysOfWeek = daysofweek
+        self.startTime = starttime
+        self.endTime = endtime
+        self.phone = phone
+        
+def make_shelter(name, address, lat, long, service_type,daysofweek,starttime,endtime,phone):
+    shelter = Shelter(name, address, lat, long, service_type,daysofweek,starttime,endtime,phone)
+    return shelter
 
 
-counter=0
+# generate a token with your client id and client secret
+TOKEN = requests.post('https://www.arcgis.com/sharing/rest/oauth2/token/', params={
+  'f': 'json',
+  'client_id': 'yrgHZpJ0ZJxstOse',
+  'client_secret': 'b9e6c0b2942e47d8a6a9098fd17a6c1c',
+  'grant_type': 'client_credentials',
+  'expiration': '1440'
+})
+#url to geocode an address
+GEOCODING_URL = 'http://geocode.arcgis.com/arcgis/rest/services/World/GeocodeServer/find'
+#url to find closest shelter/pantry
+CLOSEST_URL = 'http://route.arcgis.com/arcgis/rest/services/World/ClosestFacility/NAServer/ClosestFacility_World/solveClosestFacility'
+
+def getLatLngFromAddress(address):
+    #given an address, returns lat/lng in the form {'x': latitude, 'y': longitude}
+
+    try:
+        data = requests.post(GEOCODING_URL, params={
+            'f': 'json',
+            'token': TOKEN.json()['access_token'],
+            'text': address
+        })
+
+        data_json = data.json()
+        location = data_json.get('locations')[0] #possible locations, we just take first
+        return location.get('feature').get('geometry')
+    except Exception:
+        return None
+
+
+
+
 @app.route("/")
 def hello():
     global counter
@@ -37,7 +97,29 @@ def csv2json(filename,fieldnames):
     reader = csv.DictReader( csvfile, fieldnames)
     out = json.dumps( [ row for row in reader ] )
     return out
+
+fields=["name","address","lat","long","service_type","daysofweek","starttime","endtime","phone"]
+def shelters_csv(filename,fieldnames):
+    shelters= []
+    for line in open(filename, 'r'):
+        csv_row = line.split(sep=",") #returns a list ["1","50","60"]
+        name=csv_row[1]
+        address=csv_row[4]+","+csv_row[2]
+        lat_long=getLatLngFromAddress(address)
+        #long = lat_long['x']
+        #lat = lat_long['y']
+        lat=""
+        long=""
+        service_type=1
+        daysOfWeek=""
+        startTime=csv_row[7]
+        endTime=csv_row[8]
+        phone=""
+        shelter = make_shelter(name, address, lat, long, service_type,daysOfWeek,startTime,endTime,phone)
+        shelters.append(shelter)
+    return shelters
     
+
 
 @app.route("/shelters")
 def shelters():
@@ -50,12 +132,12 @@ def food(username):
     x= cache.set('counter', counter+1)
     return username+":"+str(counter)+":"+csv2json("dataset/FacilityDetails.csv",("Name","Brief Description","Proximity","Street Address","City","State","Zip Code","Phone Number","Web Site","Hours of Operation"))
 
-@app.route("/food/<username>")
-def results(latitude, longitude, time):
+@app.route("/food/input")
+def results(input):
     return
 
-if __name__ == "__main__":
-    app.run(debug=True)
+#if __name__ == "__main__":
+#    app.run(debug=True)
 
     
  #jsonfile.write(out)
