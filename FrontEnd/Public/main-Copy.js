3e987219e822c6d528403279d0b18c081a264e41
Chris Sullivan <codemastermd@gmail.com>
2016-05-08 09:06:51 -0400

    Finishing up stuff

--- FrontEnd/Public/main-Copy.js
+++ FrontEnd/Public/main-Copy.js
@@ -172,10 +172,8 @@ var posLong;
   }
 
   function createInfo(arr,marker){
-    var contentString ='<div id="content"><h2>' + arr[0] + '</h2><p><b>Address: </b>' + arr[5] + '</p><p><b>Phone Number: </b>' + arr[7] + '</p>'+
-                        '<div class="col-md-4 text-center"><a class="btn btn-large btn-info btn-block" style="background-color: black"href="uber://?client_id=wenxPDIJbS2ghQ7CFbk5zytwWnjiWp1b&action=setPickup&pickup=my_location&dropoff[latitude]=' + arr[1] + '&dropoff[longitude]=' + arr[2] + '&dropoff[nickname]=' + arr[0] + '">Uber</a></div></div>'
-
-                        //'<h3><a href="uber://?client_id=wenxPDIJbS2ghQ7CFbk5zytwWnjiWp1b&action=setPickup&pickup=my_location&dropoff[latitude]=' + arr[1] + '&dropoff[longitude]=' + arr[2] + '&dropoff[nickname]=' + arr[0] + '">Catch a Uber</h3></div>';
+    var contentString ='<div id="content"><h1>' + arr[0] + '</h1><p><b>Address: </b>' + arr[5] + '</p><p><b>Phone Number: </b>' + arr[7] + '</p>'+
+                        '<a href="uber://?client_id=wenxPDIJbS2ghQ7CFbk5zytwWnjiWp1b&action=setPickup&pickup=my_location&dropoff[latitude]=' + arr[1] + '&dropoff[longitude]=' + arr[2] + '&dropoff[nickname]=' + arr[0] + '">Catch a Uber</div>';
 
     var infowindow = new google.maps.InfoWindow({
         content: contentString
